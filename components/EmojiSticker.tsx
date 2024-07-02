b3a5ab3b474b034f5c702f714ede069cc9626876
kiwicarlos <carlos@kiwicredito.com>
2024-07-01 21:06:04 -0500

    [feat] STARTING PROJECT REACT NATIVE

---
+++ components/EmojiSticker.tsx
@@ -0,0 +1,19 @@
+import { View, Image } from 'react-native';
+
+export default function EmojiSticker({
+  imageSize,
+  stickerSource,
+}: {
+  imageSize: number;
+  stickerSource: any;
+}) {
+  return (
+    <View style={{ top: -350 }}>
+      <Image
+        source={stickerSource}
+        resizeMode='contain'
+        style={{ width: imageSize, height: imageSize }}
+      />
+    </View>
+  );
+}
